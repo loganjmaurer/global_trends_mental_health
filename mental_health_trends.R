f4b905bf19f690d4f56d20f496cf82d3a4327d52
loganjmaurer <loganmaurer@protonmail.com>
2024-06-22 02:12:58 -0500

    Add files via upload

---
+++ mental_health_trends.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+trends <- read.csv("Mental health Depression disorder Data.csv")
+trends <- na.omit(trends)
+trends <- trends %>%
+  dplyr::select(Entity, Year, Schizophrenia...., Bipolar.disorder...., Eating.disorders...., Anxiety.disorders...., Drug.use.disorders...., Depression...., Alcohol.use.disorders....) %>%
+  rename(sch = Schizophrenia....,
+         bipol = Bipolar.disorder....,
+         eat = Eating.disorders....,
+         anx = Anxiety.disorders....,
+         drug = Drug.use.disorders....,
+         dep = Depression....,
+         alc = Alcohol.use.disorders....)
